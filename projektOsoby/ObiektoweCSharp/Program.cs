ee1df1c58ad8a4a4ae48f985054539a98f03e6af
MichalLademann <105362390+MichalLademann@users.noreply.github.com>
2024-01-03 11:38:20 +0100

    Add files via upload

---
+++ projektOsoby/ObiektoweCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using ObiektoweCSharp;
+
+internal class Program
+{
+    static void Main()
+    {
+        person mojaOsoba = new person("Jan", "Kowalski", 'M', new DateTime(1990, 5, 15));
+
+        mojaOsoba.WyswietlInformacje();
+
+        Console.ReadKey();
+    }
+}
---
+++ projektOsoby/ObiektoweCSharp/person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObiektoweCSharp
+{
+    public class person
+    {
+
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public char Plec { get; set; }
+        public DateTime DataUrodzenia { get; set; }
+
+
+        public person(string imie, string nazwisko, char plec, DateTime dataUrodzenia)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Plec = plec;
+            DataUrodzenia = dataUrodzenia;
+        }
+
+        public void WyswietlInformacje()
+        {
+            Console.WriteLine($"Imię: {Imie}");
+            Console.WriteLine($"Nazwisko: {Nazwisko}");
+            Console.WriteLine($"Płeć: {Plec}");
+            Console.WriteLine($"Data urodzenia: {DataUrodzenia.ToShortDateString()}");
+        }
+    }
+}
+
+  
